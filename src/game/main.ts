a4ad5f483b2b55bef8b361c48b0de59233bf5cb2
Nguyen Duc Toan <125717928+duwcston@users.noreply.github.com>
2024-08-23 17:03:43 +0700

    Project setup

---
+++ src/game/main.ts
@@ -0,0 +1,57 @@
+import { Boot } from './scenes/Boot';
+import { Game as MainGame } from './scenes/Game';
+import { AUTO, Game } from 'phaser';
+import { Preloader } from './scenes/Preloader';
+// import { GameOver } from './scenes/GameOver';
+// import { MainMenu } from './scenes/MainMenu';
+import 'phaser/plugins/spine/dist/SpinePlugin';
+
+const ratio = Math.max(window.innerWidth / window.innerHeight, window.innerHeight / window.innerWidth)
+const DEFAULT_HEIGHT = 756
+const DEFAULT_WIDTH = ratio * DEFAULT_HEIGHT
+
+const config: Phaser.Types.Core.GameConfig = {
+    type: AUTO,
+    parent: 'game-container',
+    backgroundColor: '#1c1c1c',
+    dom: {
+        createContainer: true
+    },
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        width: DEFAULT_WIDTH,
+        height: DEFAULT_HEIGHT
+    },
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { x: 0, y: 0 },
+            debug: true
+        }
+    },
+    scene: [
+        Boot,
+        Preloader,
+        // MainMenu,
+        MainGame,
+        // GameOver
+    ],
+    plugins: {
+        scene: [
+            {
+                key: 'SpinePlugin',
+                plugin: window.SpinePlugin,
+                mapping: 'spine'
+            }
+        ]
+    }
+};
+
+const StartGame = (parent: string) => {
+
+    return new Game({ ...config, parent });
+
+}
+
+export default StartGame;
---
+++ src/game/scenes/Game.ts
@@ -0,0 +1,27 @@
+import { EventBus } from '../EventBus';
+import { Scene } from 'phaser';
+
+export class Game extends Scene {
+    camera: Phaser.Cameras.Scene2D.Camera;
+    background: Phaser.GameObjects.Image;
+    gameText: Phaser.GameObjects.Text;
+
+    constructor() {
+        super('Game');
+    }
+
+    create() {
+        this.camera = this.cameras.main;
+        this.camera.setBackgroundColor(0x00ff00);
+
+        this.background = this.add.image(512, 384, 'background');
+        this.background.setAlpha(0.5);
+
+
+        EventBus.emit('current-scene-ready', this);
+    }
+
+    changeScene() {
+        this.scene.start('GameOver');
+    }
+}
